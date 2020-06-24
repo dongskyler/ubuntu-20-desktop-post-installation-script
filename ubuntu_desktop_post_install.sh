f3e893677f191416ef0e2e540fb717209c05ed8a
Skyler Dong <dongskyler@gmail.com>
2020-06-24 13:07:25 -0500

    Remove VirtualBox due to agree to terms issues

--- ubuntu_desktop_post_install.sh
+++ ubuntu_desktop_post_install.sh
@@ -22,16 +22,16 @@ fi
 cd $HOME || exit 1
 
 printf "\033c"
-printf \
-"************************************************************\n\
-Don't leave yet.\n\
-We'll install VirtualBox shortly.\n\
-You need to manually select a few things.\n\
-After that, you're free.\n\
-We'll prompt you again.\n\
-************************************************************\n"
 
-sleep 5
+# printf \
+# "************************************************************\n\
+# Don't leave yet.\n\
+# We'll install VirtualBox shortly.\n\
+# You need to manually select a few things.\n\
+# After that, you're free.\n\
+# We'll prompt you again.\n\
+# ************************************************************\n"
+# sleep 5
 
 printf "Starting...\n"
 
@@ -46,31 +46,34 @@ printf "\n$BEGINNING_OF_BASHRC\n" >> $HOME/.bashrc
 printf "Updating...\n"
 sudo apt update
 
+printf "\033c"
 printf "Installing cURL...\n"
 sudo apt install -y curl
 curl --version
 
+printf "\033c"
 printf "Installing Wget...\n"
 sudo apt install -y wget
 wget --version
 
-printf "Installing VirtualBox...\n"
-wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo apt-key add -
-sudo sh -c 'printf "deb http://download.virtualbox.org/virtualbox/debian focal non-free contrib\n" >> /etc/apt/sources.list.d/virtualbox.org.list'
-sudo apt update
-printf "\033c"
-sudo apt install -y virtualbox-6.1
+# printf "Installing VirtualBox...\n"
+# wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo apt-key add -
+# sudo sh -c 'printf "deb http://download.virtualbox.org/virtualbox/debian focal non-free contrib\n" >> /etc/apt/sources.list.d/virtualbox.org.list'
+# sudo apt update
+# sudo apt install -y virtualbox-6.1
 
-printf \
-"************************************************************\n\
-Now you're free to go.\n\
-************************************************************\n"
+# printf \
+# "************************************************************\n\
+# Now you're free to go.\n\
+# ************************************************************\n"
 
 sleep 3
 
+printf "\033c"
 printf "Upgrading...\n"
 sudo apt upgrade
 
+printf "\033c"
 printf "Installing some dependencies...\n"
 sudo apt install -y \
   zlib1g-dev \
@@ -91,45 +94,57 @@ sudo apt install -y \
   dconf-cli \
   uuid-runtime
 
+printf "\033c"
 printf "Installing GnuPG...\n"
 sudo apt install -y gnupg gnupg-agent
 
+printf "\033c"
 printf "Installing Git...\n"
 sudo apt install -y git
 
+printf "\033c"
 printf "Installing Vim...\n"
 sudo apt install -y vim
 
+printf "\033c"
 printf "Installing elinks...\n"
 sudo apt install -y elinks
 
+printf "\033c"
 printf "Installing PulseAudio and PavuControl...\n"
 sudo apt install -y pulseaudio pavucontrol
 
+printf "\033c"
 printf "Installing Flameshot...\n"
 sudo apt install -y flameshot
 
+printf "\033c"
 printf "Installing ClamAV...\n"
 sudo apt install -y clamav-daemon clamtk
 
+printf "\033c"
 printf "Installing Nginx...\n"
 sudo apt install -y nginx
 
+printf "\033c"
 printf "Installing MySQL...\n"
 sudo apt install -y mysql-server
 
+printf "\033c"
 printf "Installing MongoDB Community Edition...\n"
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 printf "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse\n" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt update
 sudo apt install -y mongodb-org
 
+printf "\033c"
 printf "Installing ElasticSearch...\n"
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 printf "deb https://artifacts.elastic.co/packages/7.x/apt stable main\n" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt update
 sudo apt install -y elasticsearch
 
+printf "\033c"
 printf "Installing Docker...\n"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
@@ -139,6 +154,7 @@ sudo add-apt-repository \
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 
+printf "\033c"
 printf "Installing Visual Studio Code...\n"
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
@@ -146,6 +162,7 @@ sudo sh -c 'printf "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.micro
 sudo apt update
 sudo apt install -y code
 
+printf "\033c"
 printf "Installing Tor...\n"
 printf "deb https://deb.torproject.org/torproject.org bionic main\n" >> /etc/apt/sources.list
 printf "deb-src https://deb.torproject.org/torproject.org bionic main\n" >> /etc/apt/sources.list
@@ -154,28 +171,35 @@ gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
 sudo apt update
 sudo apt install -y tor deb.torproject.org-keyring
 
+printf "\033c"
 printf "Installing Tor Browser...\n"
 sudo apt install -y torbrowser-launcher
 
+printf "\033c"
 printf "Installing Google Chrome...\n"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P $HOME/Downloads/
 sudo apt install -y $HOME/Downloads/google-chrome-stable_current_amd64.deb
 
+printf "\033c"
 printf "Installing Slack...\n"
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.4.3-amd64.deb -P $HOME/Downloads/
 sudo apt install -y $HOME/Downloads/slack-desktop-*-amd64.deb
 
+printf "\033c"
 printf "Installing Skype...\n"
 wget https://go.skype.com/skypeforlinux-64.deb -P $HOME/Downloads/
 sudo apt install -y $HOME/Downloads/skypeforlinux-64.deb
 
+printf "\033c"
 printf "Installing Zoom...\n"
 wget https://zoom.us/client/latest/zoom_amd64.deb -P $HOME/Downloads/
 sudo apt install -y $HOME/Downloads/zoom_amd64.deb
 
+printf "\033c"
 printf "Installing Python 3...\n"
 sudo apt install -y python3-pip
 
+printf "\033c"
 printf "Installing virtualenv via pip3...\n"
 yes | sudo pip3 install virtualenv virtualenvwrapper
 
@@ -195,6 +219,7 @@ workon jupyter
 yes | sudo pip3 install -U notebook numpy pandas matplotlib
 deactivate jupyter
 
+printf "\033c"
 printf "Installing Node Version Manager...\n"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
@@ -207,6 +232,7 @@ nvm install node
 node --version
 npm --version
 
+printf "\033c"
 printf "Installing Yarn...\n"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 printf "deb https://dl.yarnpkg.com/debian/ stable main\n" | sudo tee /etc/apt/sources.list.d/yarn.list
@@ -216,6 +242,7 @@ export PATH="$PATH:/opt/yarn-[version]/bin"
 export PATH="$PATH:$(yarn global bin)"
 yarn --version
 
+printf "\033c"
 printf "Installing Ruby... This could take a while.\n"
 git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv
 printf 'export PATH="$HOME/.rbenv/bin:$PATH"\n' >> $HOME/.bashrc
@@ -229,14 +256,17 @@ rbenv global 2.7.1
 ruby -v
 gem install bundler
 
+printf "\033c"
 printf "Installing Rails...\n"
 gem install rails -v 6.0.2.2
 rbenv rehash
 rails -v
 
+printf "\033c"
 printf "Installing TexLive... This could take a while.\n"
 sudo apt install -y texlive-full
 
+printf "\033c"
 printf "Installing Zsh...\n"
 sudo apt install -y zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
@@ -251,11 +281,28 @@ printf 'source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh\n' >> 
 printf "Make Zsh the default shell.\n"
 chsh -s /bin/zsh
 
+printf "\033c"
 printf "Updating and upgrading one last time...\n"
 sudo apt update
 sudo apt upgrade
 
-printf "Adding aliases to .bashrc\n"
+printf "\033c"
+printf "Configuring the system...\n"
+
+printf "Installing terminal theme 'Earthsong'...\n"
+printf "36\n" | bash -c "$(wget -qO- https://git.io/vQgMr)"
+
+# dconf list /org/gnome/terminal/legacy/profiles:/
+# gsettings set org.gnome.Terminal.ProfilesList default ""
+
+printf "Configuring favorite apps...\n"
+gsettings set org.gnome.shell favorite-apps \
+"['org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', \
+'google-chrome.desktop', 'firefox.desktop', 'slack.desktop', \
+'Zoom.desktop', 'skypeforlinux.desktop', 'gnome-control-center.desktop']"
+
+printf "\033c"
+printf "Adding aliases\n"
 printf 'alias cls='"'"'printf "\033c"'"'" >> $HOME/.bashrc
 printf "alias grpod='git remote prune origin --dry-run'\n" >> $HOME/.bashrc
 printf "alias grpo='git remote prune origin'\n" >> $HOME/.bashrc
@@ -270,26 +317,13 @@ awk "/$BEGINNING_OF_BASHRC/{flag=1; next} /$END_OF_BASHRC/{flag=0} flag" $HOME/.
 printf "Creating some directories..."
 mkdir $HOME/Sites
 
+printf "\033c"
 printf "Cleaning up...\n"
 rm $HOME/Downloads/google-chrome-stable_current_amd64.deb
 rm $HOME/Downloads/slack-desktop-*-amd64.deb
 rm $HOME/Downloads/skypeforlinux-64.deb
 rm $HOME/Downloads/zoom_amd64.deb
 sudo apt autoremove
-
-printf "Configuring the system...\n"
-
-printf "Installing terminal theme 'Earthsong'...\n"
-printf "36\n" | bash -c "$(wget -qO- https://git.io/vQgMr)"
-
-# dconf list /org/gnome/terminal/legacy/profiles:/
-# gsettings set org.gnome.Terminal.ProfilesList default ""
-
-printf "Configuring favorite apps...\n"
-gsettings set org.gnome.shell favorite-apps \
-"['org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', \
-'google-chrome.desktop', 'firefox.desktop', 'slack.desktop', \
-'Zoom.desktop', 'skypeforlinux.desktop', 'gnome-control-center.desktop']"
 
 printf "\033c"
 printf \

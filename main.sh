06d426e002a6c8d02e93303d26a084f479d45d5c
Skyler Dong <dongskyler@gmail.com>
2020-06-25 13:27:22 -0500

    Minor updates

--- main.sh
+++ main.sh
@@ -17,92 +17,96 @@
 # https://opensource.org/licenses/MIT.
 #
 
-# Load functions from lib
-for f in ./lib/*.sh; do
-  source $f
-done
+main() {
+  # Load functions from lib
+  for f in ./lib/*.sh; do
+    source $f
+  done
 
-# Do pre-install checks
-pre_install_check
+  # Do pre-install checks
+  pre_install_check
 
-# Create some directories
-printf "Creating some directories...\n"
+  # Create some directories
+  printf "Creating some directories...\n"
 
-# localhost:8080 will be rooted here
-mkdir "$HOME/Sites"
+  # localhost:8080 will be rooted here
+  mkdir "$HOME/Sites"
 
-# Mark the beginning of our custom Bash profile
-BEGINNING_OF_BASHRC='# BEGINNING OF CUSTOM BASHRC'
-printf "\n%s\n" "$BEGINNING_OF_BASHRC" \
-  >>"$HOME/.bashrc"
+  # Mark the beginning of our custom Bash profile
+  BEGINNING_OF_BASHRC='# BEGINNING OF CUSTOM BASHRC'
+  printf "\n%s\n" "$BEGINNING_OF_BASHRC" \
+    >>"$HOME/.bashrc"
 
-# Install programs
-install_programs
+  # Install programs
+  install_programs
 
-# Configurate the system
-printf "\033c"
-printf "Configuring the system...\n"
+  # Configurate the system
+  printf "\033c"
+  printf "Configuring the system...\n"
 
-# Make sure files in $HOME/Sites are readable 
-# and excutable by root (whom Nginx, PHP, etc are run as)
-chmod -R 755 "$HOME/Sites"
+  # Make sure files in $HOME/Sites are readable
+  # and excutable by root (whom Nginx, PHP, etc are run as)
+  chmod -R 755 "$HOME/Sites"
 
-# Change the default shell from Bash to Zsh
-printf "Change the default shell from Bash to Zsh...\n"
-chsh -s /bin/zsh
+  # Change the default shell from Bash to Zsh
+  printf "Change the default shell from Bash to Zsh...\n"
+  chsh -s /bin/zsh
 
-# Configure terminal profiles
-printf "Configuring terminal profiles...\n"
+  # Configure terminal profiles
+  printf "Configuring terminal profiles...\n"
 
-# First, create a dummy profile.
-# For some reason, if we don't, the system cannot properly
-# install other profiles
-create_new_terminal_profile Dummy
+  # First, create a dummy profile.
+  # For some reason, if we don't, the system cannot properly
+  # install other profiles
+  create_new_terminal_profile Dummy
 
-# Install terminal profile 'Earthsong' and set it as default
-printf "Installing terminal profile 'Earthsong'...\n"
-printf "36\n" |
-  bash -c "$(wget -qO- https://git.io/vQgMr)"
-printf "Set 'Earthsong' as the default profile...\n"
-set_default_terminal_profile Earthsong
+  # Install terminal profile 'Earthsong' and set it as default
+  printf "Installing terminal profile 'Earthsong'...\n"
+  printf "36\n" |
+    bash -c "$(wget -qO- https://git.io/vQgMr)"
+  printf "Set 'Earthsong' as the default profile...\n"
+  set_default_terminal_profile Earthsong
 
-# Add shell aliases
-printf "\033c"
-printf "Adding a few shell aliases...\n"
-add_aliases
+  # Add shell aliases
+  printf "\033c"
+  printf "Adding a few shell aliases...\n"
+  add_aliases
 
-# Set favoriate apps to dash
-printf "Pinning favorite apps to dash...\n"
-set_favorite_apps
+  # Set favoriate apps to dash
+  printf "Pinning favorite apps to dash...\n"
+  set_favorite_apps
 
-# Update and upgrade one last time
-printf "\033c"
-printf "Updating and upgrading one last time...\n"
-sudo apt update
-sudo apt upgrade -y
+  # Update and upgrade one last time
+  printf "\033c"
+  printf "Updating and upgrading one last time...\n"
+  sudo apt update
+  sudo apt upgrade -y
 
-# Mark the end of our custom Bash profile
-END_OF_BASHRC='# END OF CUSTOM BASHRC'
-printf "\n%s\n" "$END_OF_BASHRC" \
-  >>"$HOME/.bashrc"
+  # Mark the end of our custom Bash profile
+  END_OF_BASHRC='# END OF CUSTOM BASHRC'
+  printf "\n%s\n" "$END_OF_BASHRC" \
+    >>"$HOME/.bashrc"
 
-# Copy and paste custom Bash profile configurations to Zsh profile
-printf "Copying and pasting custom configurations from .bashrc to .zshrc..."
-awk "/$BEGINNING_OF_BASHRC/{flag=1; next} /$END_OF_BASHRC/{flag=0} flag" \
-  "$HOME/.bashrc" \
-  >>"$HOME/.zshrc"
+  # Copy and paste custom Bash profile configurations to Zsh profile
+  printf "Copying and pasting custom configurations from .bashrc to .zshrc..."
+  awk "/$BEGINNING_OF_BASHRC/{flag=1; next} /$END_OF_BASHRC/{flag=0} flag" \
+    "$HOME/.bashrc" \
+    >>"$HOME/.zshrc"
 
-# Clean up
-printf "\033c"
-printf "Cleaning up...\n"
-clean_up
+  # Clean up
+  printf "\033c"
+  printf "Cleaning up...\n"
+  clean_up
 
-# Notify user of the end of this script
-printf "\033c"
-printf "\
+  # Notify user of the end of this script
+  printf "\033c"
+  printf "\
 ************************************************************\n\
 All done.\n\
 ************************************************************\n"
 
-# Reboot the computer
-reboot_countdown
+  # Reboot the computer
+  reboot_countdown
+}
+
+main
